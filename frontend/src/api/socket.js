ba52185f256adbeaab46e3c1d23bd9a00291f070
John Carlo Manalo <johncarlomanalo165@gmail.com>
2026-02-20 08:09:51 +0800

    Fix netlify build and socket connection

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socketUrl = process.env.REACT_APP_API_URL || "ws://localhost:3000";
+const socketUrl = process.env.REACT_APP_API_URL || "https://madison88-itsm-platform.onrender.com";
 let socket = null;
 
 export function getSocket() {
